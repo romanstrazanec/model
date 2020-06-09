79aae43318893be68da78ead206ff20c6b4f7ad9
roman.strazanec <roman.strazanec@uniqa.at>
2020-06-09 16:50:22 +0200

    document db provider

--- lib/src/db_provider.dart
+++ lib/src/db_provider.dart
@@ -5,22 +5,29 @@ import 'package:sqflite/sqflite.dart';
 import 'column.dart';
 import 'model.dart';
 
+/// Provides database.
 abstract class DBProvider with ChangeNotifier {
+  /// Database.
   static Database _db;
 
+  /// Implement this getter to set the database name.
   String get databaseName;
 
+  /// Implement this getter to set the database version.
   int get databaseVersion;
 
+  /// Implement this getter to provide the structure of the database.
   Map<String, List<Column>> get tables;
 
-  Future<T> Function<T>(Future<T> Function(Transaction), {bool exclusive})
-      get transaction => _db.transaction;
+  /// Returns transaction from database.
+  static Future<T> Function<T>(Future<T> Function(Transaction),
+      {bool exclusive}) get transaction => _db.transaction;
 
-  Batch get batch => _db.batch();
+  /// Returns database batch.
+  static Batch get batch => _db.batch();
 
-  /// Opens a database. Returns 0 for [FutureBuilder] to catch data.
-  Future<int> open() async {
+  /// Opens a database. Returns future expecting this provider.
+  Future<DBProvider> open() async {
     final dbPath = await getDatabasesPath();
     _db ??= await openDatabase(
       // Open only if _db null.
@@ -34,9 +41,10 @@ abstract class DBProvider with ChangeNotifier {
       onDowngrade: _onVersionChange,
     );
     assert(_db != null);
-    return 0;
+    return this;
   }
 
+  /// This method runs on database version change.
   void _onVersionChange(Database db, int oldVersion, int newVersion) {
     tables.forEach((name, cols) {
       _dropTable(db, name);
@@ -44,6 +52,7 @@ abstract class DBProvider with ChangeNotifier {
     });
   }
 
+  /// Saves the model.
   static Future<void> save(Model model) async {
     if (model.id == null) {
       model.id = await _db.transaction(
@@ -65,6 +74,7 @@ abstract class DBProvider with ChangeNotifier {
     }
   }
 
+  /// Deletes model from database.
   static Future<void> delete(Model model) async {
     if (model.id != null) {
       await _db.transaction(
@@ -78,10 +88,12 @@ abstract class DBProvider with ChangeNotifier {
     }
   }
 
+  /// Query all tables in database.
   void queryTables() async {
     for (final table in tables.keys) await queryTable(table);
   }
 
+  /// Query table in database.
   static Future<void> queryTable(String table) async {
     print('\n${table.toUpperCase()}:');
     (await _db.transaction((txn) => txn.query(table))).forEach((e) => print(e));
@@ -104,10 +116,12 @@ abstract class DBProvider with ChangeNotifier {
     db.execute('$sql);').catchError((_) {});
   }
 
+  /// Drops table.
   static void _dropTable(Database db, String table) async {
     db.execute('DROP TABLE $table;').catchError((_) {});
   }
 
-  static String columnIn(String column, List vals) =>
-      '$column IN (${vals.map((_) => '?').join(', ')})';
+  /// SQL in operator.
+  static String columnIn(String column, List values) =>
+      '$column IN (${values.map((_) => '?').join(', ')})';
 }
