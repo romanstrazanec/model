3119eed7caf5ba92b491c2f3dfe7b372f1e5aca1
roman.strazanec <roman.strazanec@uniqa.at>
2020-06-26 16:20:26 +0200

    remove unnecessary table documentation

--- lib/src/table.dart
+++ lib/src/table.dart
@@ -1,11 +1,7 @@
 import 'column.dart';
 
-/// Table.
 class Table {
-  /// Table name.
   final String name;
-
-  /// Columns.
   final List<Column> columns;
 
   const Table(this.name, this.columns);
