5effd5dba4c520f05e0ee5bf3ca384d6d684c65a
roman.strazanec <roman.strazanec@uniqa.at>
2020-06-26 16:22:06 +0200

    remove unnecessary column documentation

--- lib/src/column.dart
+++ lib/src/column.dart
@@ -1,15 +1,9 @@
 import 'constraint.dart';
 import 'sqlite_type.dart';
 
-/// Column of database.
 class Column {
-  /// Column name.
   final String name;
-
-  /// Column type.
   final SQLiteType type;
-
-  /// Additional constraints.
   final List<Constraint> constraints;
 
   const Column(this.name, this.type, [this.constraints]);
