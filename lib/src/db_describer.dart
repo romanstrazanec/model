0b4e6ff084798413dd4169c581442d854fad2a83
roman.strazanec <strazanec.roman@gmail.com>
2020-09-19 10:41:52 +0200

    database describer equal operator

--- lib/src/db_describer.dart
+++ lib/src/db_describer.dart
@@ -2,7 +2,6 @@ import 'column.dart';
 
 /// Describes the database.
 class DatabaseDescriber {
-
   /// Database name.
   final String name;
 
@@ -15,7 +14,13 @@ class DatabaseDescriber {
   DatabaseDescriber({this.name, this.version, this.tables}) {
     if (name == null || version == null || tables == null)
       throw ArgumentError('Database describer fields cannot be null.');
-    if (version < 0)
-      throw ArgumentError('Version cannot be negative.');
+    if (version < 0) throw ArgumentError('Version cannot be negative.');
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is DatabaseDescriber &&
+        other.name == name &&
+        other.version == version;
   }
 }
